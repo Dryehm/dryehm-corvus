d65d709e47a9a7861e820ca78579d4006c4dddcf
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-10-21 09:43:59 +0800

    Create dryehm.js

---
+++ scripts/corvus/dryehm.js
@@ -0,0 +1,12 @@
+Events.on(ClientLoadEvent, b  => {
+UnitTypes.corvus.speed = 3
+UnitTypes.corvus.legCount = 8
+UnitTypes.corvus.legLength = 100
+UnitTypes.corvus.legMoveSpace = .5
+UnitTypes.corvus.health = 20000000
+yes yes 20m hp
+UnitTypes.corvus.weapons.get(0).bullet.length = 1000
+UnitTypes.corvus.weapons.get(0).reload = 1
+UnitTypes.corvus.weapons.get(0).bullet.damage = 50000
+UnitTypes.corvus.weapons.get(0).bullet.width = 150
+});
